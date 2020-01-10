f9c556cdfae9d9a46398b48ea5895f970e2993ae
William Billingsley <wbilling@une.edu.au>
2020-01-10 15:57:28 +1100

    A little layout of the sidebar

--- src/main/scala/circuitsup/analog/Analog.scala
+++ src/main/scala/circuitsup/analog/Analog.scala
@@ -2,7 +2,7 @@ package circuitsup.analog
 
 import circuitsup.{CircuitsRoute, IntroRoute, Router}
 import circuitsup.templates.{Topic, YouTubeStage}
-import com.wbillingsley.veautiful.html.{<, SVG, ^}
+import com.wbillingsley.veautiful.html.{<, SVG, VHtmlNode, ^}
 import com.wbillingsley.veautiful.templates.Challenge
 import com.wbillingsley.veautiful.templates.Challenge.{Level, Stage}
 
@@ -17,6 +17,8 @@ object Analog {
     )
   }
 
+  val f = List(1, 2, 3).prependedAll[Int](Array(1, 2, 3))
+
 
   val topic = new Topic(
     name = "Currents, Voltages, and Resistances",
@@ -28,14 +30,51 @@ object Analog {
     cssClass = "circuits"
   )
 
+  val levels = Seq(
+    Level("Intro to Circuits", Seq(YouTubeStage("0bcvcJwUE-8"))),
+    Level("Kirchoff's Current Law", Seq.empty),
+    Level("Ohm's Law", Seq.empty),
+    Level("Kirchoff's Voltage Law", Seq.empty),
+    Level("Voltage Dividers", Seq.empty),
+  )
+
   val challenge = new Challenge(
-    Seq(
-      Level("Video", Seq(YouTubeStage("0bcvcJwUE-8")))
-    ),
-    header = <.div(
+    levels,
+    <.div(
       <.a(^.cls := "home-link", ^.href := Router.path(IntroRoute), symbol),
       <.span(^.cls := "challenge-name", topic.name)
-    )
+    ),
+    <.div(),
+    progessBlock()
   )
+
+
+  def progressTile(level:Level, i:Int):VHtmlNode = {
+    <.div(^.cls := "progress-level",
+      <.div(^.cls := "level-name", level.name),
+      <.div(^.cls := "stage-links",
+        for {
+          (s, j) <- level.stages.zipWithIndex
+        } yield {
+          <.a(^.cls := "stage-link",
+            <("i")(^.cls := "material-icons",
+              s.kind match {
+                case "video" => "videocam"
+                case _ => "lens"
+              }
+            )
+          )
+        }
+      )
+    )
+  }
+
+  def progessBlock():VHtmlNode = {
+    <.div(^.cls := "progress-block",
+      for {
+        (l, i) <- levels.zipWithIndex
+      } yield progressTile(l, i)
+    )
+  }
 
 }
